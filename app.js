0cc034b421d195ab0d70975d315db42dd15d5438
Mil05h <104977149+Mil05h@users.noreply.github.com>
2022-12-14 18:47:34 +0100

    mongoose update for cycle

--- app.js
+++ app.js
@@ -23,14 +23,26 @@ const reviewRoutes = require('./routes/reviews');
 const campgroundRoutes = require('./routes/campgrounds');
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const db = mongoose.connection;
 
-
-mongoose.connect(dbUrl, {
+const connectDB = async () => {
+  try {
+await mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected')
+});
+ } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Database connected')
