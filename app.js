9c6ce4af25176f0aedd413b96345f06f9daaf09d
Mil05h <104977149+Mil05h@users.noreply.github.com>
2022-12-14 18:55:29 +0100

    mongoose update for cyclic

--- app.js
+++ app.js
@@ -173,8 +173,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-const port = process.env.PORT || '3000';
-
-app.listen(port, () => {
-    console.log(`Express app is runnign on port ${port}`)
-});
+const port = process.env.PORT || 3000;
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log("listening for requests");
+    })
+})
